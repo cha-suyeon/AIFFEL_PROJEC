9a016ccb0d4f7ab4d9e2ea5749e17c4b8679542f
Suyeon Cha <84028683+cha-suyeon@users.noreply.github.com>
2022-02-27 14:02:10 +0900

    Delete Practice_Node/EX17 directory

--- Practice_Node/EX17/2.py
+++
@@ -1,4 +0,0 @@
-def _is_whitespace(c):
-    if c=="" or c=="\t" or c== "\r" or c=="\n" or ord(c)==0x202F:
-        return True
-    return False
--- Practice_Node/EX17/5.py
+++
@@ -1,9 +0,0 @@
-def _tokenize_vocab(vocab,context_words):
-    word_to_token=[]
-    context_tokens=[]
-    for (i, word) in enumerate(context_words):
-        word_to_token.append(len(context_tokens))
-        tokens=vocab.encode_as_pieces(word)
-        for token in tokens:
-            context_tokens.append(token)
-        return context_tokens, word_to_token
